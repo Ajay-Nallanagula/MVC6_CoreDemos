be3b444d38b557a18d1cb176bf573c737a83757c
Ajay Nallanagula <Ajay_Nallanagula@epam.com>
2018-03-21 16:46:29 +0530

    MvcVsMvcCore

---
+++ MvcVsMvcCore/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcVsMvcCore.Models;
+
+namespace MvcVsMvcCore.Controllers
+{
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
